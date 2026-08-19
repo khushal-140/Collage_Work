fe75c7386f25f88bc950a28032a58c5b0e4786e0
Khushal Bharat Bamaniya <khushalbharat0@gmail.com>
2026-08-19 21:38:00 +0530

    LabelEncoder

--- ML/One_Hot_Encoding.py
+++ ML/One_Hot_Encoding.py
@@ -1,1 +1,9 @@
-from sklearn.preprocessing import OneHotEncoder 
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+df=pd.read_csv("customer.csv")
+print(df)
+
+le=LabelEncoder()
+df["purush"]=le.fit_transform(df["purush"])
+
+print(df)
